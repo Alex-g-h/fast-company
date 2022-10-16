d59fb19b23109c4e86e67148af0f139e2d2881d7
Bulygin Aleksey <alex987@bk.ru>
2022-10-16 13:02:54 +0300

    User get by ID method added

--- src/app/api/fake.api/user.api.js
+++ src/app/api/fake.api/user.api.js
@@ -147,4 +147,12 @@ const fetchAll = () =>
     }, 2000);
   });
 
-export default { fetchAll };
+const getById = (id) =>
+  new Promise((resolve) => {
+    window.setTimeout(
+      users.find((user) => user._id === id),
+      1000
+    );
+  });
+
+export default { fetchAll, getById };
