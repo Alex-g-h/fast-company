ed560fa64f9e05a41e2b25d0b4a56ac5fd3b1cfd
Bulygin Aleksey <alex987@bk.ru>
2022-10-27 15:46:47 +0300

    Lesson 9. Forms + simple validation

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import TextField from "../components/textField";
 
 const Login = () => {
-  return (<>
-    <h3>Login</h3>
-  </>);
+  const [data, setData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = ({ target }) => {
+    setData((prevData) => ({
+      ...prevData,
+      [target.name]: target.value
+    }));
+  };
+
+  useEffect(() => {
+    validate();
+  }, [data]);
+
+  const validate = () => {
+    const errors = {};
+    for (const fieldName in data) {
+      if (data[fieldName].trim() === "") {
+        errors[fieldName] = `${fieldName} is required`;
+      }
+    }
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent page reload
+
+    // validation
+    const isValid = validate();
+    if (!isValid) return;
+
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Email"
+        type="email"
+        name="email"
+        value={data.email}
+        onChange={handleChange}
+        error={errors.email}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        name="password"
+        value={data.password}
+        onChange={handleChange}
+        error={errors.password}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default Login;
