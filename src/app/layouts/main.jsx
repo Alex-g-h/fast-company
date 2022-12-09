6a332728f93a32a3bef17e9ad6559891142e4e86
Bulygin Aleksey <alex987@bk.ru>
2022-12-10 00:32:39 +0300

    Init Firebase from mock data

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import useMockData from "../utils/mockData";
 
 const Main = () => {
-  return (<>
-    <h3>Main Page</h3>
-  </>);
+  const { error, initialize, progress, status } = useMockData();
+
+  const handleInit = () => {
+    initialize();
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1>Main Page</h1>
+      <h3>Firebase data initialization</h3>
+      <ul>
+        <li>Status: {status}</li>
+        <li>Progress: {progress}%</li>
+        {error && <li>Error: {error}</li>}
+      </ul>
+      <button className="btn btn-primary" onClick={handleInit}>
+        Init data
+      </button>
+    </div>
+  );
 };
 
 export default Main;
