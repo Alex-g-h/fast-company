4c408ccbe48a04e378a9ef548c9e64a6e7a049ce
Bulygin Aleksey <alex987@bk.ru>
2022-12-12 14:00:36 +0300

    Refactoring DB tokens storage through localStorage

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import axios from "axios";
 import userService from "../services/user.service";
 import { toast } from "react-toastify";
+import { setTokens } from "../services/localStorage.service";
 
 const httpAuth = axios.create();
 
@@ -11,10 +12,6 @@ const AuthContext = React.createContext();
 export const useAuth = () => {
   return useContext(AuthContext);
 };
-
-const TOKEN_KEY = "jwt-token";
-const REFRESH_KEY = "jwt-refresh-token";
-const EXPIRES_KEY = "jwt-expires";
 
 const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({});
@@ -27,13 +24,6 @@ const AuthProvider = ({ children }) => {
       setError(null);
     }
   }, [error]);
-
-  function setTokens({ refreshToken, idToken, expiresIn = 3600 }) {
-    const expiresDate = new Date().getTime() + expiresIn * 1000;
-    localStorage.setItem(TOKEN_KEY, idToken);
-    localStorage.setItem(REFRESH_KEY, refreshToken);
-    localStorage.setItem(EXPIRES_KEY, expiresDate);
-  }
 
   async function signUp({ email, password, ...rest }) {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`;
