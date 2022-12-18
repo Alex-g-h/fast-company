600d84dd3cc7aabb5c7774b34aa9469ffdab9146
Bulygin Aleksey <alex987@bk.ru>
2022-12-19 00:00:55 +0300

    Fix unsecure edit user

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
 import UserEditPage from "../components/page/userEditPage";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import UserProvider from "../hooks/useUser";
+import { useAuth } from "../hooks/useAuth";
 
 const UsersLayout = () => {
   const params = useParams();
   const { userId, edit } = params;
+
+  const { currentUser } = useAuth();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (userId && edit && edit === "edit") {
+      if (currentUser._id !== userId) {
+        history.push(`/users/${currentUser._id}/edit`);
+      }
+    }
+  }, []);
 
   return (
     <>
