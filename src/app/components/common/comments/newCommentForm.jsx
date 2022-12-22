fa9a69cc18b7f659c959d50884f2a7d506616403
Bulygin Aleksey <alex987@bk.ru>
2022-12-22 14:52:02 +0300

    Refactoring TextArea in New Comment Component

--- src/app/components/common/comments/newCommentForm.jsx
+++ src/app/components/common/comments/newCommentForm.jsx
@@ -1,24 +1,21 @@
 import React, { useState } from "react";
 import { useComment } from "../../../hooks/useComment";
+import TextAreaField from "../form/textAreaField";
 
 const NewCommentForm = () => {
   const [commentMessage, setCommentMessage] = useState("");
   const { createComment } = useComment();
 
-  const textAreaComponent = document.querySelector("#commentTextArea");
-
   const handleSendMessage = (target) => {
     createComment({
-      content: textAreaComponent?.value
+      content: commentMessage
     });
 
-    // cleaning up the form
-    textAreaComponent.value = "";
-    setCommentMessage("");
+    setCommentMessage(""); // cleaning up the form
   };
 
-  const handleTextAreaChange = ({ target }) => {
-    setCommentMessage(target.value);
+  const handleTextAreaChange = ({ value }) => {
+    setCommentMessage(value);
   };
 
   const isFormFilled = Boolean(commentMessage.trim());
@@ -28,18 +25,12 @@ const NewCommentForm = () => {
       <div className="card-body">
         <div>
           <h2>New comment</h2>
-          <div className="mb-4 align-self-right">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">
-              Message
-            </label>
-            <textarea
-              className="form-control"
-              id="commentTextArea"
-              rows="3"
-              name="comment"
-              onChange={handleTextAreaChange}
-            ></textarea>
-          </div>
+          <TextAreaField
+            label="Message"
+            name="message"
+            value={commentMessage}
+            onChange={handleTextAreaChange}
+          />
           <div className="mb-4 text-end">
             <button
               className="btn btn-primary"
