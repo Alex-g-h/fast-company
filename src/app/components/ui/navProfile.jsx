fc142d386453f3ce084a7eac1e9e6f0ed441e28c
Bulygin Aleksey <alex987@bk.ru>
2022-12-22 14:31:48 +0300

    Handle click outside navbar profile

--- src/app/components/ui/navProfile.jsx
+++ src/app/components/ui/navProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import Avatar from "../common/avatar";
@@ -11,8 +11,25 @@ const NavProfile = () => {
     setOpen((prevState) => !prevState);
   };
 
+  // add click outside event
+  useEffect(() => {
+    document.addEventListener("click", (event) => {
+      const navProfileHTML = document.querySelector("#nav-profile");
+
+      let { target } = event;
+      do {
+        if (target === navProfileHTML) return; // ignore click in this handler while clicked on ROI
+
+        target = target.parentNode; // climbing up in the DOM tree
+      } while (target);
+
+      // hide menu if clicked outside
+      setOpen(false);
+    });
+  }, []);
+
   return (
-    <div className="dropdown" onClick={toggleDropdown}>
+    <div className="dropdown" id="nav-profile" onClick={toggleDropdown}>
       <div className="btn dropdown-toggle d-flex align-items-center">
         <div className="me-2">{currentUser.name}</div>
         <Avatar
