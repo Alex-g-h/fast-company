aeaa90fe0748ae93f1e6b60fcd9c9d198f67a933
Bulygin Aleksey <alex987@bk.ru>
2023-02-24 17:33:05 +0300

    Get qualities and professions from DB, server

--- server/routes/profession.routes.js
+++ server/routes/profession.routes.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const Profession = require("../models/Profession");
 const router = express.Router({ mergeParams: true });
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const professions = await Profession.find();
+    res.status(200).send(professions);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server side error occurred. Try again later" });
+  }
+});
 
 module.exports = router;
--- server/routes/quality.routes.js
+++ server/routes/quality.routes.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const Quality = require("../models/Quality");
 const router = express.Router({ mergeParams: true });
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const qualities = await Quality.find();
+    res.status(200).send(qualities);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server side error occurred. Try again later" });
+  }
+});
 
 module.exports = router;
