7ceb23545987bcd49cfcb2e0edb27976ff913635
Bulygin Aleksey <alex987@bk.ru>
2023-02-24 17:28:35 +0300

    Fix routes filenames in require

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 
 router.use("/auth", require("./auth.routes"));
-router.use("/comment", require("./auth.comment"));
-router.use("/quality", require("./auth.quality"));
-router.use("/profession", require("./auth.profession"));
-router.use("/user", require("./auth.user"));
+router.use("/comment", require("./comment.routes"));
+router.use("/quality", require("./quality.routes"));
+router.use("/profession", require("./profession.routes"));
+router.use("/user", require("./user.routes"));
 
 module.exports = router;
