f6af5136d17db7e56a6fa052d0c64646109e681a
Bulygin Aleksey <alex987@bk.ru>
2023-02-25 00:19:22 +0300

    [chore]: Validation input data

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,35 +4,54 @@ const router = express.Router({ mergeParams: true });
 const bcrypt = require("bcryptjs");
 const { generateUserData } = require("../utils/helpers");
 const tokenService = require("../services/token.service");
+const { check, validationResult } = require("express-validator");
 
-router.post("/signUp", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const existingUser = await User.findOne({ email });
+router.post("/signUp", [
+  check("email", "Invalid e-mail").isEmail(),
+  check("password", "Minimum password length is 8 symbols").isLength({
+    min: 8,
+  }),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: {
+            message: "INVALID_DATA",
+            code: 400,
+            // errors: errors.array(),
+          },
+        });
+      }
 
-    if (existingUser) {
-      return res
-        .status(400)
-        .json({ error: { message: "EMAIL_EXISTS", code: 400 } });
+      const { email, password } = req.body;
+
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        return res
+          .status(400)
+          .json({ error: { message: "EMAIL_EXISTS", code: 400 } });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const newUser = await User.create({
+        ...generateUserData(),
+        ...req.body,
+        password: hashedPassword,
+      });
+
+      const tokens = tokenService.generate({ id: newUser._id });
+      await tokenService.save(newUser._id, tokens.refreshToken);
+
+      res.status(201).send({ ...tokens, userId: newUser._id });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Server side error occurred. Try again later" });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const newUser = await User.create({
-      ...generateUserData(),
-      ...req.body,
-      password: hashedPassword,
-    });
-
-    const tokens = tokenService.generate({ id: newUser._id });
-    await tokenService.save(newUser._id, tokens.refreshToken);
-
-    res.status(201).send({ ...tokens, userId: newUser._id });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Server side error occurred. Try again later" });
-  }
-});
+  },
+]);
 
 router.post("/signInWithPassword", async (req, res) => {});
 
