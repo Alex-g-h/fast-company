7e315b600600ac6ce3545e9a6f3ae774fee8ab5c
Bulygin Aleksey <alex987@bk.ru>
2023-02-25 12:11:16 +0300

    [chore]: User routing

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router({ mergeParams: true });
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server side error occurred. Try again later" });
+  }
+});
 
-router.patch("/:id", async (req, res) => {});
+router.patch("/:id", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    // TODO: userId === current user ID
+    if (userId) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+      });
+      res.send(updatedUser);
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server side error occurred. Try again later" });
+  }
+});
 
 module.exports = router;
